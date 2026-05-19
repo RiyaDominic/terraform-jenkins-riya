99408e53ae4c87a2996bbe99ae642d01861b3caf
RiyaDominic <108110518+RiyaDominic@users.noreply.github.com>
2026-05-19 11:23:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,45 @@
 
+provider "aws" {
+  region = "ap-south-2"
+}
+
+resource "aws_security_group" "riya_sg" {
+  name        = "terraform-riya-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "riya_bucket" {
+  bucket = "riya-terraform-bucket-demo-2026"
+}
+
+resource "aws_instance" "riya_ec2" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.riya_sg.id]
+
+  tags = {
+    Name = "Riya-Terraform-EC2"
+  }
+}
